28c9c787a6ab1c42c638b4cacc5bc8657eb6afae
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-05-19 12:00:40 +0100

    starting with DELETE keyword made more sense to me because I see it as removing student

--- java-school-database/src/main/java/com/wileyedge/database/dao/SchoolDaoImpl.java
+++ java-school-database/src/main/java/com/wileyedge/database/dao/SchoolDaoImpl.java
@@ -127,7 +127,7 @@ public class SchoolDaoImpl implements SchoolDao {
         // Write a query to remove David Mitchell as a teacher.
         // YOUR CODE STARTS HERE
 
-        String sql = "UPDATE";
+        String sql = "DELETE";
 
         // YOUR CODE ENDS HERE
         jdbcTemplate.update(sql);
