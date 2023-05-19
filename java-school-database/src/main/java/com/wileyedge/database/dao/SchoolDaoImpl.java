08d78c027425848a901c7925e1f485ddf8878906
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-05-19 11:18:50 +0100

    test passed on displaying the department and the total number of teachers assigned to each department

--- java-school-database/src/main/java/com/wileyedge/database/dao/SchoolDaoImpl.java
+++ java-school-database/src/main/java/com/wileyedge/database/dao/SchoolDaoImpl.java
@@ -62,7 +62,7 @@ public class SchoolDaoImpl implements SchoolDao {
         //  Name the aggregate field `teacherCount`.
         // YOUR CODE STARTS HERE
 
-        String sql = "";
+        String sql = "SELECT dept, COUNT(*) AS teacherCount FROM teacher GROUP BY dept";
 
         // YOUR CODE ENDS HERE
         return jdbcTemplate.query(sql, new TeacherCountMapper());
