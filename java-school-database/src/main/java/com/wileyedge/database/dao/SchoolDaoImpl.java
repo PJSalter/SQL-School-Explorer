41932af81daaca05cdc223289c70115067382e71
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-05-19 12:02:07 +0100

    now accessing to delete from teacher database

--- java-school-database/src/main/java/com/wileyedge/database/dao/SchoolDaoImpl.java
+++ java-school-database/src/main/java/com/wileyedge/database/dao/SchoolDaoImpl.java
@@ -127,7 +127,7 @@ public class SchoolDaoImpl implements SchoolDao {
         // Write a query to remove David Mitchell as a teacher.
         // YOUR CODE STARTS HERE
 
-        String sql = "DELETE";
+        String sql = "DELETE FROM teacher";
 
         // YOUR CODE ENDS HERE
         jdbcTemplate.update(sql);
