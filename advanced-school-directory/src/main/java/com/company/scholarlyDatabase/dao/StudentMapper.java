c73858bc4e84ea842b4170ee3edb00069a36fa30
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-10-23 17:00:16 +0100

    Added StudentMapper for Student Model

--- advanced-school-directory/src/main/java/com/company/scholarlyDatabase/dao/StudentMapper.java
+++ advanced-school-directory/src/main/java/com/company/scholarlyDatabase/dao/StudentMapper.java
@@ -1,6 +1,6 @@
-package com.wileyedge.database.dao;
+package com.company.scholarlyDatabase.dao;
 
-import com.wileyedge.database.model.Student;
+import com.company.scholarlyDatabase.model.Student;
 import org.springframework.jdbc.core.RowMapper;
 import java.sql.ResultSet;
 import java.sql.SQLException;
