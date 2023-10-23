b2e96f922d6d366ccf99139ff73ed7638e84c194
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-10-23 16:47:45 +0100

    Added the main application class for SchoolDBExplorer

--- advanced-school-directory/src/main/java/com/company/scholarlyDatabase/SchoolDBExplorer.java
+++ advanced-school-directory/src/main/java/com/company/scholarlyDatabase/SchoolDBExplorer.java
@@ -1,4 +1,4 @@
-package com.wileyedge.database;
+package com.company.scholarlyDatabase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
