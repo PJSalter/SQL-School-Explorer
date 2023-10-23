05c5a6774a909cbef6b44f212649ae13b9cdc57b
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-10-23 16:49:00 +0100

    Implemented SchoolController for RESTful API endpoints

--- advanced-school-directory/src/main/java/com/company/scholarlyDatabase/controller/SchoolController.java
+++ advanced-school-directory/src/main/java/com/company/scholarlyDatabase/controller/SchoolController.java
@@ -1,11 +1,10 @@
-package com.wileyedge.database.controller;
+package com.company.scholarlyDatabase.controller;
 
-import com.wileyedge.database.model.Course;
-import com.wileyedge.database.dao.SchoolDao;
-import com.company.scholarlyDatabase.model.*;
-import com.wileyedge.database.model.Student;
-import com.wileyedge.database.model.StudentClassCount;
-import com.wileyedge.database.model.TeacherCount;
+import com.company.scholarlyDatabase.model.Course;
+import com.company.scholarlyDatabase.dao.SchoolDao;
+import com.company.scholarlyDatabase.model.Student;
+import com.company.scholarlyDatabase.model.StudentClassCount;
+import com.company.scholarlyDatabase.model.TeacherCount;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
